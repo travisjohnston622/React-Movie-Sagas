7d1de8690814aa1fe8925af3d95079e3cc4e2fe7
Travis Johnston <travisjohnston4588@icloud.com>
2020-08-04 19:34:00 -0500

    updated routes in server.js

--- server/server.js
+++ server/server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
+const getMovies = require('./routes/getMovies.router');
+const putMovies = require('./routes/putMovies.router');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
-
+app.use('/getMovies', getMovies);
+app.use('/getMovies', putMovies);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
