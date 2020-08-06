c06c6d83df44c1ba881da6bf0be6b86b2dde9be3
Travis Johnston <travisjohnston4588@icloud.com>
2020-08-06 17:37:45 -0500

    putTitleReducer created

---
+++ src/redux/reducers/putTitle.reducer.js
@@ -0,0 +1,10 @@
+const putTitleReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_TITLE':
+            return action.payload;
+        default:
+            return state;
+    }
+};
+
+export default putTitleReducer;
