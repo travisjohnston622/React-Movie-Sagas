a7bbb63fda7453115ef7b63ad76553548c4a8003
Travis Johnston <travisjohnston4588@icloud.com>
2020-08-09 16:59:33 -0500

    updated routes in app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 
+//React-Router
+import { HashRouter as Router, Route } from 'react-router-dom';
+import Home from '../Pages/Home/Home';
+import Details from '../Pages/Details/Details';
+import Edit from '../Pages/Edit/Edit';
+
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
-      <div className="App">
-        <p>Empty Page</p>
-      </div>
+      <Router>
+        <div className="App"></div>
+        <Route path="/" exact component={Home} />
+        <Route path="/details/" exact component={Details} />
+        <Route path="/edit/" exact component={Edit} />
+      </Router>
     );
   }
 }
